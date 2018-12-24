e45bd6e7112655da63b0b131c21e614a2aa9171d
luxurong <luxurong@hjimi.cn>
2018-12-24 14:15:55 +0800

    init

---
+++ api/src/main/java/io/reactor/netty/api/exception/NotSupportException.java
@@ -0,0 +1,13 @@
+package io.reactor.netty.api.exception;
+
+/**
+ * @Auther: lxr
+ * @Date: 2018/12/24 14:01
+ * @Description:
+ */
+public class NotSupportException  extends RuntimeException{
+
+    public NotSupportException(String message) {
+        super(message);
+    }
+}
