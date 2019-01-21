b4a0b72253b876cda03f38bc7a5f8a8f6d11123b
1ssqq1lxr <1074455781@qq.com>
2019-01-21 23:20:25 +0800

    heart test

--- transport/src/main/java/io/rector/netty/transport/distribute/def/DefaultOffMessageHandler.java
+++ transport/src/main/java/io/rector/netty/transport/distribute/def/DefaultOffMessageHandler.java
@@ -1,6 +1,9 @@
 package io.rector.netty.transport.distribute.def;
 
 
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
 import io.reactor.netty.api.codec.ClientType;
 import io.reactor.netty.api.codec.OfflineMessage;
 import io.reactor.netty.api.codec.TransportMessage;
@@ -13,6 +16,8 @@ import reactor.core.publisher.Mono;
  */
 public class DefaultOffMessageHandler implements OffMessageHandler {
 
+    private Multimap<String, TransportMessage> messages = Multimaps.synchronizedMultimap(LinkedHashMultimap.create()); // user -> list<RConnection>
+
     /**
      *
      * @param message 离线消息 针对个人 from 来自哪里的 example 【 point -> point 】【 point -> group】
@@ -20,12 +25,11 @@ public class DefaultOffMessageHandler implements OffMessageHandler {
      */
     @Override
     public Mono<Void> storageOfflineMessage(Flux<OfflineMessage> message) {
-
-        return message.doOnNext(s->{}).then();
+        return message.doOnNext(s->messages.put(s.getUserId(),s.getMessage())).then();
     }
 
     @Override
     public Flux<TransportMessage> getToMessages(String to, ClientType clientType) {
-        return Flux.empty();
+        return Flux.just(messages.get(to).toArray(new TransportMessage[messages.get(to).size()]));
     }
 }
